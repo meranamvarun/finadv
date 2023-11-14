1b02e824de5b5c700dc78bad36db340443af17ba
varun_m_s <varun>
2023-11-15 01:56:58 +0530

    Adding fe files

---
+++ fe/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <InvestmentsTable/>
+  </div>
+</template>
+
+<script>
+
+import InvestmentsTable from './components/InvestmentsTable.vue'
+export default {
+  name: 'App',
+  components: {
+    InvestmentsTable
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fe/src/components/InvestmentsTable.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>User ID</th>
+          <th>Platform ID</th>
+          <th>Investment Type</th>
+          <th>Name</th>
+          <th>Invested Time</th>
+          <th>Annual Return Rate</th>
+          <th>Expected XOR</th>
+          <th>Volatility</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="investment in investments" :key="investment.id">
+          <td>{{ investment.id }}</td>
+          <td>{{ investment.user_id }}</td>
+          <td>{{ investment.platform_id }}</td>
+          <td>{{ investment.invest_type }}</td>
+          <td>{{ investment.name }}</td>
+          <td>{{ investment.invested_time }}</td>
+          <td>{{ investment.annual_return_rate }}</td>
+          <td>{{ investment.expected_xor }}</td>
+          <td>{{ investment.volatility }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      investments: []
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:8000/investments/')
+      .then(response => {
+        this.investments = response.data;
+      })
+      .catch(error => {
+        console.error("There was an error fetching the investments:", error);
+      });
+  }
+};
+</script>
+
+<style>
+/* Add styles for your table here */
+</style>
